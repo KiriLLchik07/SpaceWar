71ba36fb3ae18a79b125861ce32c2f90fe1d4e38
KiriLLchik07 <kirill3456777@mail.ru>
2025-03-20 19:02:09 +0600

    fix checkStyle

--- SpaceWar_Tests/RegisterIocDependencyGameRepositoryTests.cs
+++ SpaceWar_Tests/RegisterIocDependencyGameRepositoryTests.cs
@@ -1,5 +1,5 @@
-﻿using SpaceWar_workspace;
-using Hwdtech.Ioc;
+﻿using Hwdtech.Ioc;
+using SpaceWar_workspace;
 
 namespace SpaceWar_Tests
 {
@@ -13,7 +13,7 @@ namespace SpaceWar_Tests
                 IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
 
         }
-        
+       
         [Fact]
         public void RegisterIocDependencyGameRepository_RegistersDependencies_Successfully()
         {
@@ -35,7 +35,7 @@ namespace SpaceWar_Tests
                 "Ship1"
             );
             Assert.NotNull(getCommandResult);
-            
+           
             var removeCommand = IoC.Resolve<ICommand>(
                 "GameItem.Remove",
                 "Ship1"
