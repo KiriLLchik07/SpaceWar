70c53855721b8f105702829254693b289f300ca5
macbook <macbook@macbooks-MacBook-Pro.local>
2025-04-20 18:58:52 +0600

    Create GameCommand

---
+++ SpaceWar_workspace/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace SpaceWar_workspace;
+
+public class Game : ICommand
+{
+    private readonly object _gameScope;
+
+    public Game(object gameScope)
+    {
+        _gameScope = gameScope;
+    }
+
+    public void Execute()
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        IoC.Resolve<ICommand>("Scopes.Current.Set", _gameScope).Execute();
+
+        while (IoC.Resolve<bool>("Game.CanContinue", stopwatch.ElapsedMilliseconds))
+        {
+            IoC.Resolve<Action>("Game.GameBehaviour")();
+        }
+
+        stopwatch.Stop();
+    }
+}
---
+++ SpaceWar_workspace/GameBehaviourCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace SpaceWar_workspace;
+
+public class GameBehaviourCommand : ICommand
+{
+    public void Execute()
+    {
+        var command = IoC.Resolve<ICommand>("Game.Queue.Get");
+
+        try
+        {
+            command.Execute();
+        }
+        catch (Exception e)
+        {
+            IoC.Resolve<ICommand>("ExceptionHandler.Handle", command, e).Execute();
+        }
+    }
+}
